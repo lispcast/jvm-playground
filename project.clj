9fa48b4769b1c27537b0b6e27a46370c56fd6cb8
Eric Normand <ericwnormand@gmail.com>
2016-12-07 15:19:27 -0600

    Add jvm-opts

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://purelyfunctional.tv/"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :jvm-opts ["-Xmx2g" "-server" "-Xms1g"])
